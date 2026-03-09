f618c8769fe2b89647532ff626120cf7d1e52433
Younes Tsouli <younestsouli444@gmail.com>
2026-03-09 07:15:23 +0100

    small refactor

--- src/components/suggestion.jsx
+++ src/components/suggestion.jsx
@@ -2,7 +2,8 @@ import ReactMarkdown from "react-markdown"
 
 export default function Suggestion(props) {
       return (
-            <div className="other-container">
+            <div className="other-container" aria-live="polite">
+                  <h2>The Chef Recommends: </h2>
                   <div className="suggested-recipe-container">
                         <ReactMarkdown>{props.recipe}</ReactMarkdown>
                   </div>
@@ -10,21 +11,3 @@ export default function Suggestion(props) {
             
       )
 }
-
-/*
-<div className="suggestion">
-                        <h1 className="title">Suggested recipe:</h1>
-                        <p id="intro"></p>
-                  </div>
-                  <div className="other-container">
-                        <h2 className="small-title">Ingrediants:</h2>
-                        <ul className="ingredients-list">
-                              {}
-                        </ul>
-                  </div>
-                  <div className="other-container">
-                        <h2 className="small-title">Instructions:</h2>
-                        <ul className="instructions-list">
-                              {props.intructions}
-                        </ul>
-                  </div> */
